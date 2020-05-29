2f5285335468509cd14a37b473439afea1eaf2d2
Sathvika <sathvika@vpn-two-factor-general-230-148-42.dartmouth.edu>
2020-05-28 20:46:44 -0400

    made some more progress

--- frontend.py
+++ frontend.py
@@ -3,9 +3,7 @@ import os
 
 
 def user_login(url, data):
-
 	resp = requests.get(url, json=data)
-
 	if resp.json()['status'] == 401:
 		print('Something went wrong: {}'.format(resp.json()['error']))
 		exit()
@@ -14,26 +12,21 @@ def user_login(url, data):
 		exit()
 	else:
 		print('Login Succeeded {}'.format(resp.status_code))
-
 	print(resp.json())
 
 
 def login():
 	# print("Enter Username: ")
 	inputusername = input("Enter Username: ")
-
 	# print("Enter Password: ")
 	inputpass = input("Enter Password: ")
-
 	auth = {
 		"table": "Employees",
 		"username": inputusername,
 		"password": inputpass
 	}
-
 	user_login('http://localhost:3000/api/Employee/' +
 	           str(inputusername) + "/" + str(inputpass), auth)
-
 	return inputusername, inputpass
 
 
@@ -95,6 +88,33 @@ def set_Task(username, password, taskID, markTask):
 def deletes(username, password):
 	pass
 
+def add(x, username, password, fields):
+	values = []
+	data = ""
+	value = "not done"
+	i = 0
+
+	while value != "done" and i < len(fields):
+		value = input("Enter Value for " + str(fields[i]) + ": ")
+		if value != "done":
+			values.append(value)
+		i += 1
+
+	data += "?"
+	for i in range(0, len(fields)):
+		data += str(fields[i]) + "=" + str(values[i]) + "&"
+	data = data[:-1]
+
+	json_resp = make_post_call('http://localhost:3000/api/' + str(x) + '/' + str(
+		username) + '/' + str(password) + '/' + str(data), {})
+	print(json_resp)
+	
+	print()
+	print("--------------------")
+	print("Hit Enter When Done!")
+	print("--------------------")
+	y = input()
+	os.system('clear')
 
 def make_get_call(url):
 	# make get call to url
@@ -104,19 +124,17 @@ def make_get_call(url):
 		# This means something went wrong.
 		print('Something went wrong {}'.format(resp.status_code))
 		exit()
-
 	return resp.json()['response']
-
 
 def make_post_call(url, data):
 	# make post call to url passing it data
 	resp = requests.post(url, json=data)
 	# expecting to get a status of 201 on success
-	if resp.json()['status'] != 201:
+	if resp.json()['status'] != 200:
 		print('Something went wrong {}'.format(resp.status_code))
 		exit()
 	print('post succeeded')
-
+	return resp.json()['response']
 
 def make_put_call(url, data):
 	# make post call to url passing it data
@@ -126,9 +144,7 @@ def make_put_call(url, data):
 		print('Something went wrong: {}'.format(resp.json()['error']))
 		exit()
 	print('put succeeded')
-
 	return resp.json()['response']
-
 
 def make_delete_call(url):
 	# make post call to url passing it data
@@ -145,15 +161,14 @@ def use(username, password):
 	x = "not quit"
 	# response = make_get_call('http://localhost:3000/api/employees/' + str(username))
 	# admin_status = response['response'][0]['AdminRights']
-
 	admin_status = 1
 
 	print(admin_status)
 	while (x != "quit"):
 		print("You are currently in the Main panel!")
-		print("----------------------------------------")
+		print("--------------------------------------------------")
 		print("Options: ViewInfo, ModifyInfo, AddInfo, DeleteInfo")
-		print("----------------------------------------")
+		print("--------------------------------------------------")
 		x = input()
 		os.system('clear')
 
@@ -184,7 +199,6 @@ def use(username, password):
 				print("Hit Enter When Done!")
 				print("--------------------")
 				y = input()
-
 				os.system('clear')
 
 		elif(x == "ModifyInfo"):
@@ -237,7 +251,7 @@ def use(username, password):
 					option_list = ['IsEmpty', 'Purpose']
 					updates(x, username, password, objID, option_list)
 				else:
-					print("Invalid Query!")
+					print("That wasn't one of the choices. Try again!")
 					
 			
 		elif(x == "AddInfo"): # NEED TO FINISH THIS
@@ -251,20 +265,43 @@ def use(username, password):
 
 				x = input()
 				os.system('clear')
+				if (x == "Employee"):
+					fields = ['EmployeeID', 'EmployeeName', 'EmployeeTypeID', 'Salary', 'StartDate', 'Username', 'Password', 'IsAdmin']
+					add(x, username, password, fields)
+				elif(x == "EmployeeShift"):
+					fields = ['ShiftID', 'EmployeeID', 'Date']
+					add(x, username, password, fields)
+				elif(x == "EmployeeType"):
+					fields = ['EmployeeTypeID', 'TypeName', 'TypeDescription']
+					add(x, username, password, fields)
+				elif(x == "Task"):
+					fields = ['TaskID', 'TaskCodeID', 'IsComplete', 'TimeAssigned', 'RoomID', 'ShiftID']
+					add(x, username, password, fields)
+				elif(x == "TaskCode"):
+					fields = ['TaskCodeID', 'TaskName', 'TaskDescription', 'Equipment']
+					add(x, username, password, fields)
+				elif(x == "Patient"):
+					fields = ['PatientID', 'RoomID', 'PatientName', 'ArrivalDate', 'PatientDescription']
+					add(x, username, password, fields)
+				elif(x == "Room"):
+					fields = ['RoomID', 'IsEmpty', 'Purpose']
+					add(x, username, password, fields)
+				else:
+					print("That wasn't one of the choices. Try again!")
 
 		elif (x == "DeleteInfo"):  # NEED TO HANDLE ERRORS SUCH AS INVALID IDS
 			while(x != "back" and x != "quit"):
 				os.system('clear')
 				print("You are currently in the DeleteInfo panel!")
 				print("----------------------------------------")
-				print("Deletion Options: Employee, EmployeeShift, EmployeeType, Task, TaskCode, Patient, Room")
+				print("Deletion Options: Employee, ShiftType, Shift, EmployeeType, Task, TaskCode, Patient, Room")
 				print("To go back, simply enter 'back'")
 				print("----------------------------------------")
 
 				x = input()
 				os.system('clear')
-				if (x == "Employee" or x == "EmployeeShift" or x == "EmployeeType" or x == "PatientID"
-					or x == "ShiftType" or x == "Task" or x == "TaskCode" or x == "Room"):
+				if (x == "Employee" or x == "ShiftType" or x == "EmployeeType" or x == "Patient"
+					or x == "Shift" or x == "Task" or x == "TaskCode" or x == "Room"):
 					objID = input("Which " + str(x).lower() + " would you like to delete (" + str(x) + "ID): ")
 					json_resp = make_delete_call('http://localhost:3000/api/' + str(x) + '/' + str(username) + '/' + \
 						str(password) + '/' + str(objID))
@@ -277,11 +314,10 @@ def use(username, password):
 					y = input()
 
 					os.system('clear')
+				else:
+					print("That wasn't one of the choices. Try again!")
 
-
-
-				
-							
+					
 if __name__ == '__main__':
 	(userID, userpass)= login()
 	use(userID, userpass)
