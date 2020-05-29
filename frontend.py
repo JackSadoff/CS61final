c0319f9511dc0cc4cbb0b7a307eba7d679bf8eb4
Suraj Srivats <suraj.srivats.22@dartmouth.edu>
2020-05-29 02:52:40 -0700

    admin code all set

--- frontend.py
+++ frontend.py
@@ -1,6 +1,14 @@
 import requests
 import os
+import pandas as pd
+import json
 
+def read_json_custom(resp_json):
+	for row in resp_json:
+		for entry in row:
+			print(str(entry) + ": " + str(row[entry]), end="\t")
+		print()
+		print("*******************************")
 
 def user_login(url, data):
 	resp = requests.get(url, json=data)
@@ -12,7 +20,8 @@ def user_login(url, data):
 		exit()
 	else:
 		print('Login Succeeded {}'.format(resp.status_code))
-	print(resp.json())
+	
+	return resp.json()['response']
 
 
 def login():
@@ -25,8 +34,9 @@ def login():
 		"username": inputusername,
 		"password": inputpass
 	}
-	user_login('http://localhost:3000/api/Employee/' +
+	resp_json = user_login('http://localhost:3000/api/Employee/' +
 	           str(inputusername) + "/" + str(inputpass), auth)
+
 	return inputusername, inputpass
 
 
@@ -36,6 +46,7 @@ def updates(x, username, password, objID, options):
 	data = ""
 	mod = "not done"
 	new = "not done"
+	flag2 = True
 
 	while mod != "done" and new != "done":
 		mod = input("Field: ")
@@ -44,11 +55,23 @@ def updates(x, username, password, objID, options):
 		if mod != "done" and new != "done":
 			modify.append(mod)
 			change.append(new)
+		# elif mod == "done" or new == "done":
+			
+		# 	print()
+		# 	print("Invalid Entry! Please try again.")
+		# 	print("********************")
+		# 	print("Press Enter to continue.")
+		# 	input()
+		# 	return
 
 	flag = True
 	for each in modify:
 		if each not in options:
 			flag = False
+	
+	if len(modify) == 0 or len(change) == 0:
+		flag = False
+	# print(flag)
 
 	if flag:
 		data += "?"
@@ -71,6 +94,21 @@ def updates(x, username, password, objID, options):
 
 	os.system('clear')
 
+def mod_auth(x, username, password):
+	json_resp = make_get_call('http://localhost:3000/api/' + str(x) + '/' + str(username) + '/' + str(password))
+	
+	pklist = []
+	for row in json_resp:
+		for entry in row:
+			pklist.append(row[entry])
+			break
+	print("Valid Keys:", end="")
+	for i in range(len(pklist) - 1):
+		print(str(pklist[i]) + ",", end="")
+	print(str(pklist[len(pklist)-1]))
+	
+	return pklist
+
 
 def set_Task(username, password, taskID, markTask):
 	json_resp = make_put_call('http://localhost:3000/api/Complete/Task' + '/' + str(username) + '/' \
@@ -91,13 +129,14 @@ def deletes(username, password):
 def add(x, username, password, fields):
 	values = []
 	data = ""
-	value = "not done"
 	i = 0
+	value = ""
+	while i < len(fields):
+		while value == "":
+			value = input("Enter Value for " + str(fields[i]) + ": ")
+		values.append(value)
+		value = ""
 
-	while value != "done" and i < len(fields):
-		value = input("Enter Value for " + str(fields[i]) + ": ")
-		if value != "done":
-			values.append(value)
 		i += 1
 
 	data += "?"
@@ -105,15 +144,22 @@ def add(x, username, password, fields):
 		data += str(fields[i]) + "=" + str(values[i]) + "&"
 	data = data[:-1]
 
-	json_resp = make_post_call('http://localhost:3000/api/' + str(x) + '/' + str(
+	# print(data)
+	try:
+		json_resp = make_post_call('http://localhost:3000/api/' + str(x) + '/' + str(
 		username) + '/' + str(password) + '/' + str(data), {})
-	print(json_resp)
+
+		print(json_resp)
+	except:
+		print("Erroneous Entry! Please try again.")
+	
 	
 	print()
 	print("--------------------")
 	print("Hit Enter When Done!")
 	print("--------------------")
-	y = input()
+	input()
+
 	os.system('clear')
 
 def make_get_call(url):
@@ -163,7 +209,7 @@ def use(username, password):
 	# admin_status = response['response'][0]['AdminRights']
 	admin_status = 1
 
-	print(admin_status)
+	# print(admin_status)
 	while (x != "quit"):
 		print("You are currently in the Main panel!")
 		print("--------------------------------------------------")
@@ -183,15 +229,16 @@ def use(username, password):
 				print("----------------------------------------")
 				x = input()
 				os.system('clear')
-				if (x == "Employee" or x == "EmployeeShift" or x == "EmployeeType" or x == "PatientID"
+				if (x == "Employee" or x == "EmployeeShift" or x == "EmployeeType" or x == "Patient"
 					or x == "ShiftType" or x == "Task" or x == "TaskCode" or x == "Room" or x == "TaskLog" or x == "PatientLog"):
 					json_resp = make_get_call(
 					    'http://localhost:3000/api/' + str(x) + '/' + str(username) + '/' + str(password))
-					print(json_resp)
+					# print(json_resp)
+					read_json_custom(json_resp)
 				elif (x == 'back'):
 					os.system('clear')
 					break
-				else:
+				elif (x != "back" or x != "quit"):
 					print("That wasn't one of the choices! Try again.")
 
 				print()
@@ -215,45 +262,101 @@ def use(username, password):
 
 				if (x == "Employee"):
 					objID = input("Which employee would you like to update (ID): ")
-					print("Employee Modification Options: EmployeeName, EmployeeTypeID, Salary, IsAdmin")
-					option_list = ['EmployeeName', 'EmployeeTypeID', 'Salary', 'IsAdmin']
-					updates(x, username, password, objID, option_list)
+
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						print("Employee Modification Options: EmployeeName, EmployeeTypeID, Salary, IsAdmin")
+						option_list = ['EmployeeName', 'EmployeeTypeID', 'Salary', 'IsAdmin']
+						updates(x, username, password, objID, option_list)
+					else:
+						print("Invalid ID!")
+						print("*******************")
+						input("Press Enter to Continue!")
+
+
 				elif (x == "EmployeeShift"):
 					objID = input("Which shift would you like to update (ShiftID): ")
-					print("EmployeeShift Modification Options: EmployeeID, ShiftTypeID")
-					option_list = ['EmployeeID', 'ShiftTypeID']
-					updates(x, username, password, objID, option_list)
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						print("EmployeeShift Modification Options: EmployeeID, ShiftTypeID")
+						option_list = ['EmployeeID', 'ShiftTypeID']
+						updates(x, username, password, objID, option_list)
+					else:
+						print("Invalid ID!")
+						print("*******************")
+						input("Press Enter to Continue!")
+
 				elif (x == "EmployeeType"):
 					objID = input("Which employee type would you like to update (TypeID): ") 
-					print("EmployeeType Modification Options: TypeName, TypeDescription")
-					option_list = ['TypeName', 'TypeDescription']
-					updates(x, username, password, objID, option_list)
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						print("EmployeeType Modification Options: TypeName, TypeDescription")
+						option_list = ['TypeName', 'TypeDescription']
+						updates(x, username, password, objID, option_list)
+					else:
+						print("Invalid ID!")
+						print("*******************")
+						input("Press Enter to Continue!")
 				elif (x == "Patient"):
 					objID = input("Which patient would you like to update (PatientID): ")
-					print("Patient Modification Options: RoomID, PatientName, PatientDescription")
-					option_list = ['RoomID', 'PatientName', 'PatientDescription']
-					updates(x, username, password, objID, option_list)
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						print("Patient Modification Options: PatientName, PatientDescription")
+						option_list = ['PatientName', 'PatientDescription']
+						updates(x, username, password, objID, option_list)
+					else:
+						print("Invalid ID!")
+						print("*******************")
+						input("Press Enter to Continue!")
 				elif (x == "Task"):
-					taskID = input("Which task's completion status would you like to update (TaskID): ")
-					markTask = input("Type 0 if you would like to mark task as incomplete, Type 1 if you would like to mark task as complete: ")
-					while markTask != str(0) and markTask != str(1):
-						markTask = input("Please type 0 or 1 ONLY: ")
-					set_Task(username, password, taskID, markTask)
+					objID = input("Which task's completion status would you like to update (TaskID): ")
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						markTask = input("Type 0 if you would like to mark task as incomplete, Type 1 if you would like to mark task as complete: ")
+						while markTask != str(0) and markTask != str(1):
+							markTask = input("Please type 0 or 1 ONLY: ")
+						set_Task(username, password, objID, markTask)
+					else:
+						print("Invalid ID!")
+						print("*******************")
+						input("Press Enter to Continue!")
 
 				elif (x == "TaskCode"):
 					objID = input("Which task code would you like to update (TaskCodeID): ")
-					print("TaskCode Modification Options: TaskName, TaskDescription, Equipment")
-					option_list = ['TaskName', 'TaskDescription', 'Equipment']
-					updates(x, username, password, objID, option_list)
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						print("TaskCode Modification Options: TaskName, TaskDescription, Equipment")
+						option_list = ['TaskName', 'TaskDescription', 'Equipment']
+						updates(x, username, password, objID, option_list)
+					else:
+						print("Invalid ID!")
+						print("*******************")
+						input("Press Enter to Continue!")
+
+						
 				elif (x == "Room"):
 					objID = input("Which room would you like to update (RoomID): ")
-					print("Room Modification Options: IsEmpty, Purpose")
-					option_list = ['IsEmpty', 'Purpose']
-					updates(x, username, password, objID, option_list)
-				else:
+					pklist = mod_auth(x, username, password)
+					if int(objID) in pklist:
+						print("Room Modification Options: IsEmpty, Purpose")
+						option_list = ['IsEmpty', 'Purpose']
+						updates(x, username, password, objID, option_list)
+					else:
+						print("Invalid ID!")
+						input("Press Enter to Continue!")
+						
+				elif (x == "back" or x == "quit"):
+					continue
+				elif (x != "back" or x != "quit"):
 					print("That wasn't one of the choices. Try again!")
-					
+					print("--------------------")
+					print("Hit Enter To Continue!")
+
+					y = input()
+			os.system('clear')
 			
+
+
 		elif(x == "AddInfo"): # NEED TO FINISH THIS
 			while(x != "back" and x != "quit"):
 				os.system('clear')
@@ -268,6 +371,7 @@ def use(username, password):
 				if (x == "Employee"):
 					fields = ['EmployeeID', 'EmployeeName', 'EmployeeTypeID', 'Salary', 'StartDate', 'Username', 'Password', 'IsAdmin']
 					add(x, username, password, fields)
+					
 				elif(x == "EmployeeShift"):
 					fields = ['ShiftID', 'EmployeeID', 'Date']
 					add(x, username, password, fields)
@@ -286,8 +390,12 @@ def use(username, password):
 				elif(x == "Room"):
 					fields = ['RoomID', 'IsEmpty', 'Purpose']
 					add(x, username, password, fields)
-				else:
+				elif (x == "back" or x == "quit"):
+					continue
+				elif (x != "back" or x != "quit"):
 					print("That wasn't one of the choices. Try again!")
+					print("--------------------")
+					print("Hit Enter To Continue!")
 
 		elif (x == "DeleteInfo"):  # NEED TO HANDLE ERRORS SUCH AS INVALID IDS
 			while(x != "back" and x != "quit"):
@@ -297,27 +405,51 @@ def use(username, password):
 				print("Deletion Options: Employee, ShiftType, Shift, EmployeeType, Task, TaskCode, Patient, Room")
 				print("To go back, simply enter 'back'")
 				print("----------------------------------------")
-
+				objID = ""
 				x = input()
 				os.system('clear')
 				if (x == "Employee" or x == "ShiftType" or x == "EmployeeType" or x == "Patient"
 					or x == "Shift" or x == "Task" or x == "TaskCode" or x == "Room"):
-					objID = input("Which " + str(x).lower() + " would you like to delete (" + str(x) + "ID): ")
-					json_resp = make_delete_call('http://localhost:3000/api/' + str(x) + '/' + str(username) + '/' + \
-						str(password) + '/' + str(objID))
-					print(json_resp)
+
+					while objID == "":
+						objID = input("Which " + str(x).lower() + " would you like to delete (" + str(x) + "ID): ")
+
+					pklist = mod_auth(x, username, password)
+					# print(pklist)
+					
+					if int(objID) in pklist:
+						try:
+							json_resp = make_delete_call('http://localhost:3000/api/' + str(x) + '/' + str(username) + '/' + \
+								str(password) + '/' + str(objID))
+							print(json_resp)
+						except:
+							print("Erroneous Entry! Please try again.")
+							print("For reference here is a list of valid delete ID's")
+					else:		
+						print("Invalid ID!")
+
+					
 
 					print()
 					print("--------------------")
-					print("Hit Enter When Done!")
+					print("Hit Enter To Continue!")
 					print("--------------------")
 					y = input()
 
 					os.system('clear')
-				else:
+				elif (x == "back" or x == "quit"):
+					continue
+				elif (x != "back" or x != "quit"):
 					print("That wasn't one of the choices. Try again!")
+					print("--------------------")
+					print("Hit Enter To Continue!")
+
+					y = input()
+			os.system('clear')
+
 
 					
 if __name__ == '__main__':
 	(userID, userpass)= login()
 	use(userID, userpass)
+	# print(resp_json)
